4cc08965f369a8a9c672963137c90652088b79c8
alex_nedelsky <alex.nedelsky@gmail.com>
2023-07-20 00:41:26 +0200

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
+
+ReactDOM.render(
+    <DevSupport ComponentPreviews={ComponentPreviews}
+                useInitialHook={useInitial}
+    >
+        <App/>
+    </DevSupport>,
+    document.getElementById('root')
+);
